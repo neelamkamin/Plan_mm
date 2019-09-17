7a8516e86a9f2082a0e3b79acfe4801d58c43487
neelamkamin <55264874+neelamkamin@users.noreply.github.com>
2019-09-17 11:00:19 +0530

    Update fund_detail.php

--- application/views/pub/fund_detail.php
+++ application/views/pub/fund_detail.php
@@ -1,21 +1,19 @@
 <?php include_once('header.php'); ?>
-<!------END !------->
-
 <div class="container">
 	<table>
 	<div class="row">
 		<div class="col-lg-6">
 		<td>
-			<a href="<?php echo site_url("Admin") ?>" class="btn btn-lg btn-primary">DEPT-WISE LIST</a>
+			<a href="<?php echo site_url("Auth") ?>" class="btn btn-lg btn-primary">DEPT-WISE LIST</a>
 		</td>	
 		<td>
-		<a href="<?php echo site_url('user') ?>" class="btn btn-lg btn-primary">SHOW ALL PROJECTS LIST</a>
+		<a href="<?php echo site_url('Admin') ?>" class="btn btn-lg btn-primary">SHOW ALL PROJECTS LIST</a>
 		</td>
 		<td>
 		<a href="<?php echo site_url('Admin/dashboard') ?>" class="btn btn-lg btn-primary">DEPT DASHBOARD</a>
 		</td>
 		 <td>
-            <a href="<?php echo site_url("User/extra") ?>" class="btn btn-lg btn-primary">Dept. Wise Project</a>
+            <a href="<?php echo site_url("Admin/extra") ?>" class="btn btn-lg btn-primary">Dept. Wise Project</a>
         </td>
 		</div>
 	</div>
