505e5dfac543ce9442702efbcc222fca866afe94
neelamkamin <55264874+neelamkamin@users.noreply.github.com>
2021-03-23 12:28:25 +0530

    Update Schemes_model.php

--- application/models/Schemes_model.php
+++ application/models/Schemes_model.php
@@ -1,19 +1,57 @@
 <?php
 class Schemes_model extends CI_Model {
 
-/*var $articles refers to schemes only 
-	Word article = scheme
+/*var $schemes refers to schemes only 
+	Word scheme = scheme
 Actually this code is reuse version of my earlier project AND I didn't change the variables name
-*/
+*/ 
+public function count_all_dept()
+/*This Model function will load on landing page if user is admin */
+	{
+		//$dept_id = $this->session->userdata('dept_id');
+		$query = $this->db
+							->select(['name_scheme','id'])
+							->from('schemes')
+							->group_by('dept_id')
+							->get();
+		return $query->num_rows();
+	}
+
+	public function sum_column($limit, $offset)
+/*This Model function will also load on landing page if user is admin */	
+	{
+		$this->db->select('dept_id, SUM(budget_est) AS AMOUNT, COUNT(name_scheme) AS TOTAL ', FALSE);
+		$this->db->where('status', 1);
+		$this->db->group_by('dept_id');
+		$this->db->limit ($limit, $offset);
+		$this->db->order_by('id','DESC');
+		$query = $this->db->get('schemes');
+		return $query->result_array();
+
+	}
+
+	public function num_rows() //THIS NEW FUNCTION IS MADE TO GET 'total_rows' FOR PAGINATION, SEE LINE 14 OF ADMIN.PHP FILE //
+	{
+		$dept_id = $this->session->userdata('dept_id');
+		//print_r($dept_id); exit;
+		$query = $this->db
+							->select()
+							->from('schemes')
+							//->where('dept_id', $dept_id)
+							->where(['dept_id'=>$dept_id, 'status'=> 0])
+							->get();
+		return $query->num_rows();
+	}
 
 	public function schemes_list($limit, $offset)
 	//THIS FUNCTION IS TO SHOW LIST OF PROJECTS ON DASHBOARD//
 	{
-		$user_id = $this->session->userdata('user_id');
+		$dept_id = $this->session->userdata('dept_id');
 		$query = $this->db
 		                    ->select()
 							->from('schemes')
-							->where('user_id', $user_id)
+							//->where('dept_id', $dept_id)
+							->where(['dept_id'=>$dept_id, 'status'=> 0])
 							->limit ($limit, $offset) //FOR PAGINATION ONLY//
 							->order_by('id','DESC')
 							->get();
@@ -29,6 +67,7 @@ Actually this code is reuse version of my earlier project AND I didn't change th
 		$query = $this->db
 							->select()
 							->from('schemes')
+							->where('status', 1)
 							->limit ($limit, $offset) //FOR PAGINATION ONLY//
 							->order_by('id','DESC')
 							->get();			
@@ -38,40 +77,18 @@ Actually this code is reuse version of my earlier project AND I didn't change th
 
 	public function count_all_schemes()
 	{
-		$user_id = $this->session->userdata('user_id');
+		$dept_id = $this->session->userdata('dept_id');
 		$query = $this->db
-							->select(['user_id','id'])
+							->select(['dept_id','id'])
 							->from('schemes')
 							->get();
 		return $query->num_rows();
-	}
-
-	public function count_all_dept()
-	{
-		$user_id = $this->session->userdata('user_id');
-		$query = $this->db
-							->select(['name_scheme','id'])
-							->from('schemes')
-							->group_by('user_id')
-							->get();
-		return $query->num_rows();
-	}
-
-
-	public function sum_column($limit, $offset)
-	{
-		$this->db->select('user_id, SUM(budget_est) AS AMOUNT, COUNT(name_scheme) AS TOTAL ', FALSE);
-		$this->db->group_by('user_id');
-		$this->db->limit ($limit, $offset);
-		$this->db->order_by('id','DESC');
-		$query = $this->db->get('schemes');
-		return $query->result_array();
-
 	}
 
 	public function sum_category($limit, $offset)
 	{
 		$this->db->select('fund_cat, SUM(budget_est) AS AMOUNT', FALSE);
+		$this->db->where('status', 1);
 		$this->db->group_by('fund_cat');
 		$this->db->limit ($limit, $offset);
 		$this->db->order_by('id','DESC');
@@ -81,7 +98,7 @@ Actually this code is reuse version of my earlier project AND I didn't change th
 	}
 	public function count_category()
 	{
-		$user_id = $this->session->userdata('user_id');
+		$dept_id = $this->session->userdata('dept_id');
 		$query = $this->db
 							->select(['name_scheme','id'])
 							->from('schemes')
@@ -93,7 +110,8 @@ Actually this code is reuse version of my earlier project AND I didn't change th
 	public function sum_scheme($limit, $offset)
 	{
 		$this->db->select('category, SUM(budget_est) AS AMOUNT', FALSE);
-		//$this->db->select("user_id, SUM(budget_est) as AMOUNT");
+		//$this->db->select("dept_id, SUM(budget_est) as AMOUNT");
+		$this->db->where('status', 1);
 		$this->db->group_by('category');
 		$this->db->limit ($limit, $offset);
 		$this->db->order_by('id','DESC');
@@ -103,7 +121,7 @@ Actually this code is reuse version of my earlier project AND I didn't change th
 	}
 	public function count_scheme()
 	{
-		$user_id = $this->session->userdata('user_id');
+		$dept_id = $this->session->userdata('dept_id');
 		$query = $this->db
 							->select(['name_scheme','id'])
 							->from('schemes')
@@ -118,46 +136,34 @@ Actually this code is reuse version of my earlier project AND I didn't change th
 		return $this->db->insert('schemes',$array);
 	}
 
-	public function find_scheme($article_id)
+	public function find_scheme($scheme_id)
 	{
 		$q = $this->db
 			     //->select(['id','name_scheme','body','status','remark','budget_est','exp','category'])
 				->select()
-				->where('id',$article_id)
+				->where('id',$scheme_id)
 				->get('schemes');
 
 		return $q->row(); //HERE WE RETURN 'ROW'TO FETCH ONLY ONE AFFECTED ROW, IF WE RETURN 'RESULT' THEN ARRAY WILL START WITH ZERO (0) INDEX// 
 	}
 
-	public function update_scheme($article_id, Array $article)
+	public function update_scheme($scheme_id, Array $scheme)
 	{
 		return $this->db
-				->where('id',$article_id)
-				->update('schemes',$article);
+				->where('id',$scheme_id)
+				->update('schemes',$scheme);
 	}
 
 	public function delete_scheme($arr_id)
 	{
-		return $this->db->delete('schemes',['id'=>$arr_id]); //HERE 1ST PARAMETER IS TABLE NAME, i.e->schemes & 2ND PARAMETER IS ID OF THE REPORT-ARTICLE TO BE DELETED//
-	}
-
-	
-
-	public function num_rows() //THIS NEW FUNCTION IS MADE TO GET 'total_rows' FOR PAGINATION, SEE LINE 14 OF ADMIN.PHP FILE //
-	{
-		$user_id = $this->session->userdata('user_id');
-		$query = $this->db
-							->select(['name_scheme','id'])
-							->from('schemes')
-							->where('user_id', $user_id)
-							->get();
-		return $query->num_rows();
+		return $this->db->delete('schemes',['id'=>$arr_id]); //HERE 1ST PARAMETER IS TABLE NAME, i.e->schemes & 2ND PARAMETER IS ID OF THE REPORT-scheme TO BE DELETED//
 	}
 
 	public function search( $query, $limit, $offset )//HERE WE RECEIVED PARAMETER "$query" FROM 'public function search()' OF 'user.php' FILE AT LINE 39//
 	{
 		$q = $this->db->from('schemes')
-					->like('user_id',$query)//HERE WE PASSED "$query" WHICH WE RECEIVED ABOVE AS PARAMETER//
+					->like('name_scheme',$query)//HERE WE PASSED "$query" WHICH WE RECEIVED ABOVE AS PARAMETER//
+					->where('status', 1)
 					->limit ( $limit, $offset)
 					->order_by('id','DESC')
 					->get();
@@ -167,33 +173,39 @@ Actually this code is reuse version of my earlier project AND I didn't change th
 	public function count_search_results($query)
 	{
 		$qr = $this->db->from('schemes')
-						->like('user_id',$query)
+						->like('name_scheme',$query)
+						->where('status', 1)
 						->get();
 		return $qr->num_rows();
 	}
 
-	public function dept_pagination( $user_id, $limit, $offset )
-//TO DISPLAY DATA WITH PAGINATION OF CONTROLLER "User/dept_result($user_id)" VIA ANCHOR TAG/LINK ONLY AT VIEW FILE:-  "single_dept.php"//
+	public function dept_pagination( $dept_id, $limit, $offset )
+//TO DISPLAY DATA WITH PAGINATION OF CONTROLLER "User/dept_result($dept_id)" VIA ANCHOR TAG/LINK ONLY AT VIEW FILE:-  "single_dept.php"//
 	{
 	
-		$this->db->select('user_id,category, SUM(budget_est) AS AMOUNT, COUNT(name_scheme) AS TOTAL ', FALSE);
-		//$this->db->select("user_id, SUM(budget_est) as AMOUNT");
+		$this->db->select('dept_id,category, SUM(budget_est) AS AMOUNT, COUNT(name_scheme) AS TOTAL ', FALSE);
+		//$this->db->select("dept_id, SUM(budget_est) as AMOUNT");
 		$this->db->group_by('category');
-		$this->db->where( ['user_id' => $user_id] );
+
+		$this->db->where( [
+							'status' => 1, 
+							'dept_id' => $dept_id
+						 ] );
+
 		$this->db->limit ($limit, $offset);
 		$this->db->order_by('id','DESC');
 		$query = $this->db->get('schemes');
 		return $query->result();
 	}
 
-	public function find_dept ( $user_id )//TO FIND 'user_id' VIA ANCHOR tag/link only//
+	public function find_dept ( $dept_id )//TO FIND 'dept_id' VIA ANCHOR tag/link only//
 	{
 	
 		$query = $this->db
-							->select(['name_scheme','id','user_id'])
+							->select(['name_scheme','id','dept_id'])
 							->from('schemes')
 							->group_by('category')
-							->where( ['user_id' => $user_id] )
+							->where( ['dept_id' => $dept_id] )
 							->get();
 		return $query->num_rows();
 
@@ -212,8 +224,8 @@ Actually this code is reuse version of my earlier project AND I didn't change th
 //TO DISPLAY DATA WITH PAGINATION OF CONTROLLER "User/category_result($category)" VIA ANCHOR TAG/LINK ONLY AT VIEW FILE:-  "cat_list.php"//
 	{
 		$q = $this->db->from('schemes')
-					->like('category',$category)//HERE WE PASSED "$user_id" WHICH WE RECEIVED ABOVE AS PARAMETER//
-		            ->where( ['category' => $category] )
+					->like('category',$category)//HERE WE PASSED "$dept_id" WHICH WE RECEIVED ABOVE AS PARAMETER//
+		            ->where( ['category' => $category, 'status' => 1] )
 					->limit ( $limit, $offset)
 					->order_by('id','DESC')
 					->get();
@@ -224,7 +236,7 @@ Actually this code is reuse version of my earlier project AND I didn't change th
 	{
 		$q = $this->db
 					->from('schemes')
-					->where( ['fund_cat' => $fund] )
+					->where( ['fund_cat' => $fund, 'status' => 1] )
 					->get();
 		return $q->num_rows();
 
@@ -233,8 +245,8 @@ Actually this code is reuse version of my earlier project AND I didn't change th
 //TO DISPLAY DATA WITH PAGINATION OF CONTROLLER "User/category_result($category)" VIA ANCHOR TAG/LINK ONLY AT VIEW FILE:-  "cat_list.php"//
 	{
 		$q = $this->db->from('schemes')
-					->like('fund_cat',$fund)//HERE WE PASSED "$user_id" WHICH WE RECEIVED ABOVE AS PARAMETER//
-		            ->where( ['fund_cat' => $fund] )
+					->like('fund_cat',$fund)//HERE WE PASSED "$dept_id" WHICH WE RECEIVED ABOVE AS PARAMETER//
+		            ->where( ['fund_cat' => $fund, 'status' => 1] )
 					->limit ( $limit, $offset)
 					->order_by('id','DESC')
 					->get();
@@ -254,29 +266,31 @@ Actually this code is reuse version of my earlier project AND I didn't change th
 	
 	public function find_dept_category ()
 	{
-		$category =  $this->uri->segment(3); /*HERE WE GET USER_ID AND CATEGORY FROM URL */
-        $user_id =  $this->uri->segment(4);
-        //print_r($user_id);
+		$category =  $this->uri->segment(3); /*HERE WE GET dept_id AND CATEGORY FROM URL */
+        $dept_id =  $this->uri->segment(4);
+        //print_r($dept_id);
         //print_r($category); die();		
 		$query = $this->db
-							->select(['name_scheme','id','user_id'])
+							->select(['name_scheme','id','dept_id'])
 							->from('schemes')
 							->where( [
+										'status'  => 1,
 										'category' => $category,
-										'user_id' => $user_id
+										'dept_id' => $dept_id
 									] )
 							->get();
 		return $query->num_rows();
 
 	}
-	public function dept_category_pagination( $category,$user_id, $limit, $offset )
+	public function dept_category_pagination( $category,$dept_id, $limit, $offset )
 //TO DISPLAY DATA WITH PAGINATION OF CONTROLLER "User/category_result($category)" VIA ANCHOR TAG/LINK ONLY AT VIEW FILE:-  "cat_list.php"//
 	{
 		$q = $this->db->from('schemes')
 					->like('category',$category)//HERE WE PASSED "$category" WHICH WE RECEIVED ABOVE AS PARAMETER//
 		            ->where( [
+								'status'  => 1,
 								'category' => $category,
-								'user_id' => $user_id
+								'dept_id' => $dept_id
 							] )
 					->limit ( $limit, $offset)
 					->order_by('id','DESC')
